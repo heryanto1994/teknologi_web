30a2509e6e16511e8287b6f5711df0f43745c4e8
heryanto1994 <37211934+heryanto1994@users.noreply.github.com>
2018-08-30 14:56:03 +0700

    Update hapus.php

--- hapus.php
+++ hapus.php
@@ -3,5 +3,5 @@ include_once 'koneksi.php';
 $id = $_GET['id'];
 $sql = "DELETE FROM artikel  WHERE id = '{$id}'";
 $result = mysqli_query($conn, $sql);
-header('location: index.php?mod=admin/index');
+header('location: artikel.php?mod=admin/index');
 ?>
